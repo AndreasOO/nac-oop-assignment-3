3a928c3b53da740dc58f8f4c60195b8a02159318
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-10-29 20:07:03 +0100

    removed test code in production source code in GUI object

--- src/GUIObject/GUI.java
+++ src/GUIObject/GUI.java
@@ -50,14 +50,6 @@ public class GUI {
         populateTiles();
 
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        resetGame();
-
 
     }
 
