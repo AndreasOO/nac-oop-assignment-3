b77d89369ea0f98256b46952b8f783f78a866a63
viktorjonasson <viktorjonasson@gmail.com>
2024-10-30 14:14:48 +0100

    Completed tests for tile adjacency method.

--- test/TileObject/TileTest.java
+++ test/TileObject/TileTest.java
@@ -1,12 +1,24 @@
 package TileObject;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TileTest {
 
+    Tile testTile1 = new Tile(false, 1);
+    Tile testTile2 = new Tile(false, 2);
+    Tile testTile3 = new Tile(false, 3);
+    Tile testTileEmpty = new Tile(true, 4);
+
     @Test
     void checkAdjecencyToEmtpyTile() {
+        testTile1.assignLocation(2, 3);
+        testTile2.assignLocation(4, 1);
+        testTile3.assignLocation(4, 3);
+        testTileEmpty.assignLocation(3, 3);
+
+        assertTrue(testTile1.checkAdjecencyToEmtpyTile(testTileEmpty);
+        assertTrue(testTile3.checkAdjecencyToEmtpyTile(testTileEmpty);
+        assertFalse(testTile2.checkAdjecencyToEmtpyTile(testTileEmpty);
     }
 }
