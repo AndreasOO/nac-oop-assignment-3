56edb9c71077c8bb7e9fe56dcecdb1b2ef96b7af
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-10-31 15:03:19 +0100

    removed redundant imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import GUIObject.GUI;
-import TileObject.Tile;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
